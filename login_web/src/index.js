3a920904e1090517b548be99a7c5498817f7cc2a
Imène ALLOUCJE <https://github.com/Imeneallouche>
2023-03-06 18:31:12 +0100

    login/register page frontend has been finalized

--- login_web/src/index.js
+++ login_web/src/index.js
@@ -1,13 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-//import reportWebVitals from "./reportWebVitals";
+import ReactDOM from "react-dom";
+import ApolloProvider from "./ApolloProvider";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-//reportWebVitals();
+ReactDOM.render(ApolloProvider, document.getElementById("root"));
--- login_web/src/pages/Login.js
+++ login_web/src/pages/Login.js
@@ -1,3 +1,100 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Button, Form } from "semantic-ui-react";
-import { useMutation } from "@appolo/react-hooks";
+import { useMutation } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+
+import { AuthContext } from "../context/auth";
+import { useForm } from "../util/hooks";
+
+function Login(props) {
+  const context = useContext(AuthContext);
+  const [errors, setErrors] = useState({});
+  const [attempts, setAttempts] = useState(0);
+
+  const { onChange, onSubmit, values } = useForm(loginUserCallback, {
+    userName: "",
+    password: "",
+  });
+
+  const [loginUser, { loading }] = useMutation(LOGIN_USER, {
+    update(_, { data: { login: userData } }) {
+      context.login(userData);
+      props.history.push("/");
+    },
+    onError(err) {
+      setErrors(err.graphQLErrors[0].extensions.exception.errors);
+    },
+    variables: values,
+  });
+
+  function loginUserCallback() {
+    loginUser();
+  }
+
+  useEffect(() => {
+    if (errors.general) {
+      setAttempts(attempts + 1);
+    }
+  }, [errors, attempts]);
+
+  return (
+    <div className="form-container">
+      <Form
+        onSubmit={onSubmit}
+        noValidate
+        className={loading ? "Carregando..." : ""}
+      >
+        <h1>Login</h1>
+        <Form.Input
+          label="Username"
+          placeholder="Digite o username"
+          name="userName"
+          type="text"
+          value={values.userName}
+          error={errors.userName ? true : false}
+          onChange={onChange}
+        />
+        <Form.Input
+          label="Password"
+          placeholder="Digite a senha"
+          name="password"
+          type="password"
+          value={values.password}
+          error={errors.password ? true : false}
+          onChange={onChange}
+        />
+        <Button type="submit" disabled={attempts === 3} primary>
+          Login
+        </Button>
+        {attempts === 3 && (
+          <div className="error-message">
+            Você alcançou o máximo de tentativas.
+          </div>
+        )}
+      </Form>
+      {Object.keys(errors).length > 0 && (
+        <div className="ui error message">
+          <ul className="list">
+            {Object.values(errors).map((value) => (
+              <li key={value}>{value}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
+const LOGIN_USER = gql`
+  mutation login($userName: String!, $password: String!) {
+    login(userName: $userName, password: $password) {
+      id
+      email
+      userName
+      createdAt
+      token
+    }
+  }
+`;
+
+export default Login;
